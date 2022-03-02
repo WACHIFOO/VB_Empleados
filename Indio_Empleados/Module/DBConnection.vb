4ac33b2379f4e480fa56df778fb4a693a255565f
WACHIFOO <gallegoruizdaniel@gmail.com>
2022-03-02 12:48:37 +0100

    Primera Clase - Conexion a base de datos

--- Indio_Empleados/Module/DBConnection.vb
+++ Indio_Empleados/Module/DBConnection.vb
@@ -1,3 +1,9 @@
-﻿Module DBConnection
+﻿Imports MySql.Data.MySqlClient
 
+Module DBConnection
+    Public Function strstconnection() As MySqlConnection
+        Return New MySqlConnection("server=localhost;user id=root;database=employeedb")
+    End Function
+
+    Public strcon As MySqlConnection = strstconnection()
 End Module
